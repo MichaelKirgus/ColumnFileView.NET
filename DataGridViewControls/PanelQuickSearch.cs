7aaa107a39d1ed4f7d647420646dc0d057b34161
MichaelKirgus <git.michael@kirgus.net>
2019-01-11 15:42:31 +0100

    Add additional license notices at the license headers

--- DataGridViewControls/PanelQuickSearch.cs
+++ DataGridViewControls/PanelQuickSearch.cs
@@ -2,6 +2,7 @@
 // This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with this program; if not, see <https://www.gnu.org/licenses>.
+// This source code was initial published by "Chris_McGrath" under the "The Code Project Open License (CPOL) 1.022" license. You can find a copy under \License\License_CPOL.txt or under https://www.codeproject.com/info/cpol10.aspx
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
